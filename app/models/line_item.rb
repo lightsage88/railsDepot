146939775d566f85d9218a456b94243f8cdf86dd
lightsage88 <adrian.e.rosales@gmail.com>
2018-11-07 16:10:52 -0800

    working on the table display of the cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,15 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  def total_price
+    product.price * quantity
+    #If you look in the schema, you will notice that 
+    #Product has a price key,
+    #LineItem has a quantity key
+    #LineItem as a class is linked up with both Product and Cart
+    #as a result, in this scope we can pull Product and Cart like magic,
+    #but for anything that isn't innately of the LineItem schema, in this scope,
+    #we will need to prefix the 'price' on line 5 with 'product'
+  end
+
 end
